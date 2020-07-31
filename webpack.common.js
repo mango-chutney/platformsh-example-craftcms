495468e7e664b09f719b4c1723bb3e38658770f9
Jason Di Benedetto <jason.dibenedetto6@gmail.com>
2020-07-31 11:25:41 +0930

    setup

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: [path.resolve('./src/main.tsx')],
+    main: [path.resolve('./src/main.ts')],
   },
   output: {
     path: path.resolve(__dirname, './web/assets/'),
